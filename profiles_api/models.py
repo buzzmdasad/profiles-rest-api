cf625253a3632c5dfb4f8a6d29065a08b87f7797
Md Asaduzzaman <asad.md@tcs.com>
2024-08-15 19:59:13 +0530

    enable user for jango admin profile

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -17,7 +17,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('User must have an email address:')
 
 
-        email = self.normalize_emain(email)
+        email = self.normalize_email(email)
         user = self.model(email=email, name=name)
 
         user.set_password(password)
@@ -27,14 +27,14 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """craete and save a new super user with teh given details"""
         user=self.create_user(email,name,password)
         user.is_superuser=True
         user.is_staff=True
         user.save(using=self._db)
 
-        return users
+        return user
 
 
 
