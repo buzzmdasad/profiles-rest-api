200544ee3704c06a7a956eca6a0d2f505d7a1a09
Md Asaduzzaman <asad.md@tcs.com>
2024-08-16 19:23:40 +0530

    Added user profile feed api

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -8,3 +8,11 @@ class UpdateOwnProfile(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.id==request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """allow to ad his own feed"""
+    def has_object_permission(self,request,view,obj):
+        """check user is trying to his own ststus"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id==request.user.id
--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -27,3 +27,9 @@ def update(self, instance, validated_data):
             instance.set_password(password)
 
         return super().update(instance, validated_data)
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """Serializes profile feed item"""
+    class Meta:
+        model=models.ProfileFeedItem
+        fields=('id','user_profile','status_text','created_on')
+        extra_kwargs={'user_profile':{'read_only':True}}
